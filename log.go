276685c890d8ab50cd57253af577b9fde05a4896
Ewen Le Bihan <hey@ewen.works>
2024-10-12 13:06:37 +0200

    ✨ Improve progress bar, add time tracking, handle disabling coloring, add file writing

--- log.go
+++ log.go
@@ -4,18 +4,63 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/mattn/go-isatty"
 	"github.com/mitchellh/colorstring"
 )
 
-func logWriter() io.Writer {
-	var writer io.Writer = os.Stderr
+// Set to a path to log to a file as well as stdout.
+var LogFilePath string
+
+// Set to true to prepend the date to logs.
+var PrependDateToLogs = false
+
+var showingTimingLogs = os.Getenv("DEBUG_TIMING") != ""
+
+func logWriter(original io.Writer) io.Writer {
+	writer := original
+	if LogFilePath != "" {
+		logfile, err := os.OpenFile(LogFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err == nil {
+			writer = io.MultiWriter(writer, logfile)
+		}
+	}
+
+	if PrependDateToLogs {
+		writer = prependDateWriter{out: writer}
+	}
+
 	if progressBars != nil {
 		writer = progressBars.Bypass()
 	}
+	if !ShowingColors() {
+		writer = noAnsiCodesWriter{out: writer}
+	}
 	return writer
+}
+
+type prependDateWriter struct {
+	out io.Writer
+}
+
+func (w prependDateWriter) Write(p []byte) (n int, err error) {
+	return w.out.Write([]byte(
+		fmt.Sprintf("[%s] %s",
+			time.Now(),
+			strings.TrimLeft(string(p), " "),
+		)))
+}
+
+// noAnsiCodesWriter is an io.Writer that writes to the underlying writer, but strips ANSI color codes beforehand
+type noAnsiCodesWriter struct {
+	out io.Writer
+}
+
+func (w noAnsiCodesWriter) Write(p []byte) (n int, err error) {
+	return w.out.Write(stripansicolors(p))
 }
 
 func indentSubsequent(size int, text string) string {
@@ -25,7 +70,21 @@ func indentSubsequent(size int, text string) string {
 
 // Log logs a custom message with a verb and color.
 func Log(verb string, color string, message string, fmtArgs ...interface{}) {
-	fmt.Fprintln(logWriter(), colorstring.Color(fmt.Sprintf("[bold][%s]%15s[reset] %s", color, verb, indentSubsequent(15+1, fmt.Sprintf(message, fmtArgs...)))))
+	LogNoFormatting(verb, color, colorstring.Color(fmt.Sprintf(message, fmtArgs...)))
+}
+
+// LogNoColor logs a message without applying colorstring syntax to message.
+func LogNoColor(verb string, color string, message string, fmtArgs ...interface{}) {
+	LogNoFormatting(verb, color, fmt.Sprintf(message, fmtArgs...))
+}
+
+func LogNoFormatting(verb string, color string, message string) {
+	fmt.Fprintln(
+		logWriter(os.Stderr),
+		colorstring.Color(fmt.Sprintf("[bold][%s]%15s[reset]", color, verb))+
+			" "+
+			indentSubsequent(15+1, message),
+	)
 }
 
 // Error logs non-fatal errors.
@@ -54,9 +113,43 @@ func Debug(message string, fmtArgs ...interface{}) {
 	Log("Debug", "magenta", message, fmtArgs...)
 }
 
+// DebugNoColor logs debug information without applying colorstring syntax to message.
+func DebugNoColor(message string, fmtArgs ...interface{}) {
+	if os.Getenv("DEBUG") == "" {
+		return
+	}
+	LogNoColor("Debug", "magenta", message, fmtArgs...)
+}
+
 // Warn logs warnings.
 func Warn(message string, fmtArgs ...interface{}) {
 	Log("Warning", "yellow", message, fmtArgs...)
+}
+
+// Timing logs timing debug logs. Mostly used with TimeTrack. To enable, set DEBUG_TIMING environment variable.
+func Timing(job string, args []interface{}, timeTaken time.Duration) {
+	if !showingTimingLogs {
+		return
+	}
+	formattedArgs := ""
+	for i, arg := range args {
+		if i > 0 {
+			formattedArgs += " "
+		}
+		formattedArgs += fmt.Sprintf("%v", arg)
+	}
+	Log("Timing", "dim", "[bold]%-30s[reset][dim]([reset]%-50s[dim])[reset] took [yellow]%s", job, formattedArgs, timeTaken)
+}
+
+// TimeTrack logs the time taken for a function to execute, and logs out the time taken.
+// Usage: at the top of your function, defer TimeTrack(time.Now(), "your job name").
+// To enable, set DEBUG_TIMING environment variable.
+func TimeTrack(start time.Time, job string, args ...interface{}) {
+	if !showingTimingLogs {
+		return
+	}
+	elapsed := time.Since(start)
+	Timing(job, args, elapsed)
 }
 
 // List formats a list of strings with a format string and separator.
@@ -87,4 +180,24 @@ func FormatErrors(err error) string {
 
 func isInteractiveTerminal() bool {
 	return isatty.IsTerminal(os.Stdout.Fd()) && isatty.IsTerminal(os.Stderr.Fd())
+}
+
+func stripansicolors(b []byte) []byte {
+	// TODO find a way to do this without converting to string
+	s := string(b)
+	s = regexp.MustCompile(`\x1b\[[0-9;]*m`).ReplaceAllString(s, "")
+	return []byte(s)
+}
+
+// ShowingColors returns true if colors (ANSI escape codes) should be printed.
+// Environment variables can control this: NO_COLOR=1 disables colors, and FORCE_COLOR=1 forces colors.
+// Otherwise, heuristics (such as whether the output is an interactive terminal) are used.
+func ShowingColors() bool {
+	if os.Getenv("NO_COLOR") == "1" {
+		return false
+	}
+	if os.Getenv("FORCE_COLOR") == "1" {
+		return true
+	}
+	return isInteractiveTerminal()
 }
--- progress.go
+++ progress.go
@@ -2,6 +2,7 @@ package labellogger
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
@@ -17,7 +18,7 @@ func StartProgressBar(total int, verb string, color string) {
 		panic("progress bar already started")
 	}
 
-	if isInteractiveTerminal() {
+	if isInteractiveTerminal() || os.Getenv("FORCE_PROGRESS_BAR") == "1" {
 		Debug("terminal is interactive, starting progress bar")
 	} else {
 		Debug("not starting progress bar because not in an interactive terminal")
@@ -31,30 +32,27 @@ func StartProgressBar(total int, verb string, color string) {
 	progressbar.Fill = '='
 	progressbar.Head = '>'
 	progressbar.Width = 30
-	progressbar.PrependFunc(func(b *uiprogress.Bar) string {
-		return colorstring.Color(
-			fmt.Sprintf(
-				`[magenta][bold]%15s[reset]`,
-				"Building",
-			),
-		)
-	})
-	progressbar.AppendFunc(func(b *uiprogress.Bar) string {
-		return fmt.Sprintf("%d/%d", b.Current(), b.Total)
-	})
+	progressbar.PrependFunc(makeProgressBarPrependFunc(verb, color))
+	progressbar.AppendFunc(makeProgressBarAppendFunc(""))
 	progressBars.Start()
 }
 
-func IncrementProgress(onDone func()) {
-	if progressbar == nil {
-		return
+func IncrementProgressBar(onDone ...func()) {
+	if ProgressBarFinished() {
+		if len(onDone) > 0 {
+			onDone[0]()
+		}
+		StopProgressBar()
 	}
 
 	progressbar.Incr()
-	if progressbar.CompletedPercent() >= 100 {
-		StopProgressBar()
-		onDone()
+}
+
+func ProgressBarFinished() bool {
+	if progressbar == nil {
+		return false
 	}
+	return progressbar.CompletedPercent() >= 100
 }
 
 func StopProgressBar() {
@@ -68,25 +66,32 @@ func StopProgressBar() {
 	fmt.Print("\r\033[K")
 }
 
-func padVerb(verb string) string {
-	return fmt.Sprintf("%15s", verb)
+func UpdateProgressBar(verb string, color string, message string, details ...string) {
+	if progressbar == nil {
+		return
+	}
+
+	progressbar.PrependFunc(makeProgressBarPrependFunc(verb, color))
+	progressbar.AppendFunc(makeProgressBarAppendFunc(message, details...))
 }
 
-// Status updates the current progress
-func UpdateProgressBar(verb string, color string, message string, details ...string) {
-	formattedDetails := ""
-	if len(details) > 0 {
-		formattedDetails = fmt.Sprintf(" [dim]%s[reset]", strings.Join(details, " "))
-	}
-	formattedMessage := colorstring.Color(fmt.Sprintf("[bold][%s]%s[reset]"+formattedDetails, color, padVerb(verb), message))
-
-	if progressBars != nil {
-		fmt.Fprintln(progressBars.Bypass(), formattedMessage)
-	} else {
-		if isInteractiveTerminal() {
-			fmt.Println(formattedMessage)
-		} else {
-			fmt.Printf(" %s %s %s\n", padVerb(verb), message, strings.Join(details, " "))
+func makeProgressBarPrependFunc(verb string, color string) func(*uiprogress.Bar) string {
+	return func(b *uiprogress.Bar) string {
+		if ShowingColors() {
+			return colorstring.Color(
+				fmt.Sprintf(`[%s][bold]%s[reset]`, color, padVerb(verb)),
+			)
 		}
+		return padVerb(verb)
 	}
+}
+
+func makeProgressBarAppendFunc(message string, details ...string) func(*uiprogress.Bar) string {
+	return func(b *uiprogress.Bar) string {
+		return fmt.Sprintf("%d/%d %s [dim]%s[reset]", b.Current(), b.Total, message, strings.Join(details, " "))
+	}
+}
+
+func padVerb(verb string) string {
+	return fmt.Sprintf("%15s", verb)
 }
